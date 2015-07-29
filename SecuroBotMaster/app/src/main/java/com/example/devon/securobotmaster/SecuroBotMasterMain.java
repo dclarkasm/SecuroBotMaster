520e48c1d6920bf3207e14ec5d9b55b0be838092
dclarkasm <dclar4@unh.newhaven.edu>
2015-07-28 20:09:12 -0400

    proved that we can interface with IOIO and send messages back and forth via simultaneously

--- SecuroBotMaster/app/src/main/java/com/example/devon/securobotmaster/SecuroBotMasterMain.java
+++ SecuroBotMaster/app/src/main/java/com/example/devon/securobotmaster/SecuroBotMasterMain.java
@@ -3,12 +3,39 @@ package com.example.devon.securobotmaster;
 import com.example.devon.securobotmaster.util.SystemUiHider;
 
 import android.annotation.TargetApi;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Message;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+import android.widget.ViewAnimator;
+
+import ioio.lib.api.DigitalOutput;
+import ioio.lib.api.IOIO;
+import ioio.lib.api.exception.ConnectionLostException;
+import ioio.lib.util.BaseIOIOLooper;
+import ioio.lib.util.IOIOLooper;
+import ioio.lib.util.android.IOIOActivity;
 
 
 /**
@@ -18,7 +45,8 @@ import android.view.View;
  * @see SystemUiHider
  */
 //some comment
-public class SecuroBotMasterMain extends Activity {
+public class SecuroBotMasterMain extends IOIOActivity {
+    private ToggleButton button_;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -47,11 +75,46 @@ public class SecuroBotMasterMain extends Activity {
      */
     private SystemUiHider mSystemUiHider;
 
+    // Whether the Log Fragment is currently shown
+    private boolean mLogShown;
+
+
+
+    private static final String TAG = "BluetoothChatFragment";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+
+    /**
+     * Name of the connected device
+     */
+    private String mConnectedDeviceName = null;
+
+    /**
+     * String buffer for outgoing messages
+     */
+    private StringBuffer mOutStringBuffer;
+
+    /**
+     * Local Bluetooth adapter
+     */
+    private BluetoothAdapter mBluetoothAdapter = null;
+
+    /**
+     * Member object for the chat services
+     */
+    private BluetoothChatService mChatService = null;
+
+    Button send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_securo_bot_master_main);
+        button_ = (ToggleButton) findViewById(R.id.button);
 
         final View controlsView = findViewById(R.id.fullscreen_content_controls);
         final View contentView = findViewById(R.id.fullscreen_content);
@@ -110,11 +173,263 @@ public class SecuroBotMasterMain extends Activity {
             }
         });
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+
+//**************************************************************************************************
+                                    //Set up Bluetooth stuff
+//**************************************************************************************************
+        // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            this.finish();
+        }
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            // Initialize the BluetoothChatService to perform bluetooth connections
+            mChatService = new BluetoothChatService(this, mHandler);
+
+            // Initialize the buffer for outgoing messages
+            mOutStringBuffer = new StringBuffer("");
+        }
+
+        send = (Button) findViewById(R.id.send_button);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendMessage("This is a test message!");
+            }
+        });
+
+        //at this point you could make the device discoverable, but lets just assume
+        //we are using already paired devices for security reasons.
+        //this is a cyber security robot anyway ;)
+        //ensureDiscoverable();
+
+        //then prompt the user to select a device tot connect to (secure method)
+        Intent serverIntent = new Intent(this, DeviceListActivity.class);
+        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+
+        /* insecure method
+        Intent serverIntent = new Intent(getActivity(), DeviceListActivity.class);
+        startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+         */
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (mChatService != null) {
+            mChatService.stop();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
+    /**
+     * Makes this device discoverable.
+     */
+    private void ensureDiscoverable() {
+        if (mBluetoothAdapter.getScanMode() !=
+                BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+            startActivity(discoverableIntent);
+        }
+    }
+
+    /**
+     * Sends a message.
+     *
+     * @param message A string of text to send.
+     */
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            Toast.makeText(this, "No devices connected.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+            //mOutEditText.setText(mOutStringBuffer);
+        }
+    }
+
+    /**
+     * Updates the status on the action bar.
+     *
+     * @param resId a string resource ID
+     */
+    private void setStatus(int resId) {
+        /*
+        final ActionBar actionBar = this.getActionBar();
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(resId);
+        */
+    }
+
+    /**
+     * Updates the status on the action bar.
+     *
+     * @param subTitle status
+     */
+    private void setStatus(CharSequence subTitle) {
+        final ActionBar actionBar = this.getActionBar();
+        /*
+        if (null == actionBar) {
+            return;
+        }
+        actionBar.setSubtitle(subTitle);
+        */
+    }
+
+
+    /**
+     * The Handler that gets information back from the BluetoothChatService
+     */
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            //FragmentActivity activity = getActivity();
+            switch (msg.what) {
+                /*
+                case Constants.MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothChatService.STATE_CONNECTED:
+                            setStatus("Connected to " + mConnectedDeviceName);
+                            //mConversationArrayAdapter.clear();
+                            break;
+                        case BluetoothChatService.STATE_CONNECTING:
+                            setStatus("Connecting...");
+                            break;
+                        case BluetoothChatService.STATE_LISTEN:
+                        case BluetoothChatService.STATE_NONE:
+                            setStatus("Not connected.");
+                            break;
+                    }
+                    break;
+                    */
+                case Constants.MESSAGE_WRITE:
+                    byte[] writeBuf = (byte[]) msg.obj;
+                    // construct a string from the buffer
+                    String writeMessage = new String(writeBuf);
+                    //mConversationArrayAdapter.add("Me:  " + writeMessage);
+                    Log.d("Bluetooth", "Wrote message: " + writeMessage);
+                    Toast.makeText(SecuroBotMasterMain.this, "Wrote message: " + writeMessage, Toast.LENGTH_SHORT).show();
+                    break;
+                case Constants.MESSAGE_READ:
+                    byte[] readBuf = (byte[]) msg.obj;
+                    // construct a string from the valid bytes in the buffer
+                    String readMessage = new String(readBuf, 0, msg.arg1);
+                    //mConversationArrayAdapter.add(mConnectedDeviceName + ":  " + readMessage);
+                    Log.d("Bluetooth", "Read message: " + readMessage);
+                    Toast.makeText(SecuroBotMasterMain.this, "Read message: " + readMessage, Toast.LENGTH_SHORT).show();
+                    break;
+                case Constants.MESSAGE_DEVICE_NAME:
+                    // save the connected device's name
+                    mConnectedDeviceName = msg.getData().getString(Constants.DEVICE_NAME);
+                        Toast.makeText(SecuroBotMasterMain.this, "Connected to "
+                                + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                    break;
+                case Constants.MESSAGE_TOAST:
+                        Toast.makeText(SecuroBotMasterMain.this, msg.getData().getString(Constants.TOAST),
+                                Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    };
+
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE_SECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, true);
+                }
+                break;
+            case REQUEST_CONNECT_DEVICE_INSECURE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    connectDevice(data, false);
+                }
+                break;
+            case REQUEST_ENABLE_BT:
+                // When the request to enable Bluetooth returns
+                if (resultCode == Activity.RESULT_OK) {
+                    // Bluetooth is now enabled, so set up a chat session
+                    // Initialize the BluetoothChatService to perform bluetooth connections
+                    mChatService = new BluetoothChatService(this, mHandler);
+
+                    // Initialize the buffer for outgoing messages
+                    mOutStringBuffer = new StringBuffer("");
+                } else {
+                    // User did not enable Bluetooth or an error occurred
+                    Log.d(TAG, "BT not enabled");
+                    Toast.makeText(this, "Bluetooth was not enebled. Leaving chat",
+                            Toast.LENGTH_SHORT).show();
+                    this.finish();
+                }
+        }
+    }
+
+    /**
+     * Establish connection with other divice
+     *
+     * @param data   An {@link Intent} with {@link DeviceListActivity#EXTRA_DEVICE_ADDRESS} extra.
+     * @param secure Socket Security type - Secure (true) , Insecure (false)
+     */
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras()
+                .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -127,11 +442,11 @@ public class SecuroBotMasterMain extends Activity {
     }
 
 
-    /**
-     * Touch listener to use for in-layout UI controls to delay hiding the
-     * system UI. This is to prevent the jarring behavior of controls going away
-     * while interacting with activity UI.
-     */
+
+     //Touch listener to use for in-layout UI controls to delay hiding the
+     //system UI. This is to prevent the jarring behavior of controls going away
+     //while interacting with activity UI.
+
     View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -150,12 +465,128 @@ public class SecuroBotMasterMain extends Activity {
         }
     };
 
-    /**
-     * Schedules a call to hide() in [delay] milliseconds, canceling any
-     * previously scheduled calls.
-     */
+     //Schedules a call to hide() in [delay] milliseconds, canceling any
+     //previously scheduled calls.
     private void delayedHide(int delayMillis) {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
+    }
+
+    /**
+     * This is the thread on which all the IOIO activity happens. It will be run
+     * every time the application is resumed and aborted when it is paused. The
+     * method setup() will be called right after a connection with the IOIO has
+     * been established (which might happen several times!). Then, loop() will
+     * be called repetitively until the IOIO gets disconnected.
+     */
+    class Looper extends BaseIOIOLooper {
+        /** The on-board LED. */
+        private DigitalOutput led_;
+
+        /**
+         * Called every time a connection with IOIO has been established.
+         * Typically used to open pins.
+         *
+         * @throws ConnectionLostException
+         *             When IOIO connection is lost.
+         *
+         * //@see ioio.lib.util.IOIOLooper#setup()
+         */
+        @Override
+        protected void setup() throws ConnectionLostException {
+            showVersions(ioio_, "IOIO connected!");
+            led_ = ioio_.openDigitalOutput(0, true);
+            enableUi(true);
+        }
+
+        /**
+         * Called repetitively while the IOIO is connected.
+         *
+         * @throws ConnectionLostException
+         *             When IOIO connection is lost.
+         * @throws InterruptedException
+         * 				When the IOIO thread has been interrupted.
+         *
+         * @see ioio.lib.util.IOIOLooper#loop()
+         */
+        @Override
+        public void loop() throws ConnectionLostException, InterruptedException {
+            led_.write(!button_.isChecked());
+            Thread.sleep(100);
+        }
+
+        /**
+         * Called when the IOIO is disconnected.
+         *
+         * @see ioio.lib.util.IOIOLooper#disconnected()
+         */
+        @Override
+        public void disconnected() {
+            enableUi(false);
+            toast("IOIO disconnected");
+        }
+
+        /**
+         * Called when the IOIO is connected, but has an incompatible firmware version.
+         *
+         * @see ioio.lib.util.IOIOLooper#incompatible(IOIO)
+         */
+        @Override
+        public void incompatible() {
+            showVersions(ioio_, "Incompatible firmware version!");
+        }
+    }
+
+    /**
+     * A method to create our IOIO thread.
+     *
+     * @see ioio.lib.util.AbstractIOIOActivity#createIOIOThread()
+     */
+    @Override
+    protected IOIOLooper createIOIOLooper() {
+        return new Looper();
+    }
+
+    private void showVersions(IOIO ioio, String title) {
+        toast(String.format("%s\n" +
+                        "IOIOLib: %s\n" +
+                        "Application firmware: %s\n" +
+                        "Bootloader firmware: %s\n" +
+                        "Hardware: %s",
+                title,
+                ioio.getImplVersion(IOIO.VersionType.IOIOLIB_VER),
+                ioio.getImplVersion(IOIO.VersionType.APP_FIRMWARE_VER),
+                ioio.getImplVersion(IOIO.VersionType.BOOTLOADER_VER),
+                ioio.getImplVersion(IOIO.VersionType.HARDWARE_VER)));
+    }
+
+    private void toast(final String message) {
+        final Context context = this;
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private int numConnected_ = 0;
+
+    private void enableUi(final boolean enable) {
+        // This is slightly trickier than expected to support a multi-IOIO use-case.
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (enable) {
+                    if (numConnected_++ == 0) {
+                        button_.setEnabled(true);
+                    }
+                } else {
+                    if (--numConnected_ == 0) {
+                        button_.setEnabled(false);
+                    }
+                }
+            }
+        });
     }
 }
